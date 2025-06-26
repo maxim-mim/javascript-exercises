23950d15301ebdf7c809cce84e4e204dfa7b9719
Maxim <157955718+maxim-mim@users.noreply.github.com>
2025-06-26 19:56:48 +0200

    Complete JS Challenges 6-8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rmList) {
+
+    for (let item of rmList) {
+       let rmIndex = arr.indexOf(item);
+
+       if (rmIndex === -1) {
+            continue;
+       }
+
+        do {
+            arr.splice(rmIndex, 1);
+            rmIndex = arr.indexOf(item);
+        } while (rmIndex !== -1)
+        
+    }
+    // console.log(arr);
+    // return arr;
 };
+
+
+// let array = [1, 2, 3, 4];
+// removeFromArray(array, 3);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
