e2c1b6dea4c7e6a3d552da0082436ecab7774c73
Maxim <157955718+maxim-mim@users.noreply.github.com>
2025-07-02 00:11:35 +0200

    Complete Odin Project practice challenges 6-13

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result = 0;
+    let big, small;
 
+    if ((!(Number.isInteger(a) && Number.isInteger(b))) || (a < 0 || b < 0)) {
+        return "ERROR";
+    }
+
+    if (a > b) {
+        big = a;
+        small = b;
+    } else {
+        big = b;
+        small = a;
+    }
+
+    for (let i = small; i <= big; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,37 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    
+
+    let arr = string.split("");
+
+    let toClear = [",", "!", ".", " "]
+
+    for (let i = 0; i <= arr.length; i++) {
+        for (let char of toClear) {
+            if (arr[i] === char) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    let normal = arr.join("").toLowerCase();
+
+    let backwards = arr.reverse().join("").toLowerCase();
+
+    console.log(normal);
+    console.log(backwards);
+
+
+
+    if (normal === backwards) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes("racecar"))
